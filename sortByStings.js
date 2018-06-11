d3bc311488b0c4e1a1cb18a2932ae5242ed37e63
amzad hossain <amzad.hossain@baruchmail.cuny.edu>
2018-06-11 16:21:30 -0400

    work on first challenge

---
+++ sortByStings.js
@@ -0,0 +1,21 @@
+function sortByStrings(s,t){
+  let str = ""; //Declare str as an empty string
+  const obj = {}; //An object with property and value pair where property=current letter and value=number
+
+  //creating object with data about used letters and  their muber of occurence
+  for (let i =0; i<s.length; i++){
+    obj[s.charAt(i)] ? obj[s.charAt(i)]++ : obj[s.charAt(i)] = 1;
+  }
+
+  //This loop will generate str
+  for (let i = 0; i<t.length; i++){
+    while(obj[t.charAt(i)]>0){
+      for (let j = 0; j < obj[t.charAt(i)]; j++){
+          str = str + t.charAt(i);
+          obj[t.charAt(i)]--;}
+      }
+  }
+  return str;
+}
+
+sortByStrings("good", "odg");

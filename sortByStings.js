f8f6b898c31b941f1c497638af415af08a786d7e
Amzad Hossain <amzad.hossain@baruchmail.cuny.edu>
2018-06-12 11:51:51 -0400

    Update sortByStings.js

--- sortByStings.js
+++ sortByStings.js
@@ -2,7 +2,7 @@ function sortByStrings(s,t){
   let str = ""; //Declare str as an empty string
   const obj = {}; //An object with property and value pair where property=current letter and value=number
 
-  //creating object with data about used letters and  their muber of occurence
+  //create object with data of used letters and their muber of occurence
   for (let i =0; i<s.length; i++){
     obj[s.charAt(i)] ? obj[s.charAt(i)]++ : obj[s.charAt(i)] = 1;
   }

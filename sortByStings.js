61df94bb0a19fe14c9f5b5f790ae0742bae6cc7c
amzad hossain <amzad.hossain@baruchmail.cuny.edu>
2018-06-12 12:09:34 -0400

    solution for the sortByStrings challenge

--- sortByStings.js
+++ sortByStings.js
@@ -1,13 +1,11 @@
+// Question 1 -- sortByStrings(s,t): Sort the letters in the string s by the order they occur in the string t. You can assume t will not have repetitive characters. For s = "weather" and t = "therapyw", the output should be sortByString(s, t) = "theeraw". For s = "good" and t = "odg", the output should be sortByString(s, t) = "oodg".
+
 function sortByStrings(s,t){
   let str = ""; //Declare str as an empty string
   const obj = {}; //An object with property and value pair where property=current letter and value=number
-
-  //creating object with data about used letters and  their muber of occurence
   for (let i =0; i<s.length; i++){
     obj[s.charAt(i)] ? obj[s.charAt(i)]++ : obj[s.charAt(i)] = 1;
   }
-
-  //This loop will generate str
   for (let i = 0; i<t.length; i++){
     while(obj[t.charAt(i)]>0){
       for (let j = 0; j < obj[t.charAt(i)]; j++){
